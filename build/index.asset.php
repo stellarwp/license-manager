d2fe8da36df60903287be69addf96b8719168726
nikolaystrikhar <4025589+nikolaystrikhar@users.noreply.github.com>
2026-03-23 13:49:53 +0000

    Build frontend assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '0192c32b8784d4d421f7');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'fc20bbd33a12b3b3eab7');
