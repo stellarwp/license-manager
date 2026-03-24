c1fa9de30de1c70cedf328a826f87b861f7b3760
jonwaldstein <10138447+jonwaldstein@users.noreply.github.com>
2026-03-24 14:12:30 +0000

    Build frontend assets

--- build-dev/index.asset.php
+++ build-dev/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'f95bfcf50556ee6ab0a1');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'c011b4d1b834af4d4fc6');
