6b8581fca3fb1b146705748f4f549db2ff1ff5f7
d4mation <7770631+d4mation@users.noreply.github.com>
2026-04-08 19:26:09 +0000

    Build frontend assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '68caeae31c816c0681b9');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '3b6be0e1b2742b5e73c8');
