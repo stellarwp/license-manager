8dbf06bd0c5328adc2d19e39d454fd07bcfb00f0
pauloiankoski <3921017+pauloiankoski@users.noreply.github.com>
2026-04-08 20:14:56 +0000

    Build frontend assets

--- build-dev/index.asset.php
+++ build-dev/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'f178b62e84c1c86a771b');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '66e4087a49913e6c6cbb');
