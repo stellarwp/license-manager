c6dc524ffa9d0915fd843699b0f92cf7e458953a
dave-green-uk <3214928+dave-green-uk@users.noreply.github.com>
2026-04-24 15:36:29 +0000

    Build frontend assets

--- build-dev/index.asset.php
+++ build-dev/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ade7d9ccdca5294de9fb');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'db8d8d188e0dae34c6b9');
